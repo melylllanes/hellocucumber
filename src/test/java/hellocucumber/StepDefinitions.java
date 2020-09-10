89294f457afb6365fe688157f9a3b160839c92b9
diana.estrada <diana.estrada.hdez@gmail.com>
2020-09-10 16:36:27 -0500

    adding process builder

--- src/test/java/hellocucumber/StepDefinitions.java
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -1,7 +1,9 @@
 package hellocucumber;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,7 +57,14 @@ public class StepDefinitions {
         //p.command(command);   
         try {
         	p.directory(new File(command));
-			p.start();
+			//p.start();
+			BufferedReader reader = new BufferedReader(
+                  new InputStreamReader(p.start().getInputStream()));
+			String line = null;
+          while ((line = reader.readLine()) != null) {
+              System.out.println(line);
+          }
+          reader.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
